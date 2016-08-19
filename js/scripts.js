192e5531cef8f9c84e98a118926e482ca2923f61
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-08-19 10:23:08 -0400

    add js to test for numbers divisible by 3 and return ping if true

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 // BUSINESS LOGIC
-  var testPingPong = function(num) {
-
+var testPingPong = function(num) {
+  // var variableName would be defined here
 
   // Do nothing to non-numeric input (i.e. letters and special characters) Form-control
   // Do nothing to 0
@@ -9,23 +9,26 @@ $(document).ready(function() {
     alert("Please enter a number (must be greater than 0)");
   }
   else {
-    alert("This number passes");
-    return num;
+    // NUMBERS DIVISIBLE BY 3 ARE REPLACED WITH "ping"
+    if (num % 3 === 0) {
+      return "ping";
+    }
+    else {
+      return num;
+    }
+    // NUMBERS DIVISIBLE BY 5 ARE REPLACED WITH "pong".
+    // NUMBERS DIVISIBLE BY 15 ARE REPLACED WITH "ping-pong".
 
-    // Numbers divisible by 3 are replaced with "ping"
-
-    // Numbers divisible by 5 are replaced with "pong".
-
-    // Numbers divisible by 15 are replaced with "ping-pong".
-
+    // ALL OTHER NUMBERS THAT ARE NOT DIVISIBLE BY 3, 5, or 15 ARE RETURNED
+    // console.log(num);
+    // return num;
   }
-
 
 }
   // USER INTERFACE LOGIC
   $("#user-input").submit(function(event) {
     event.preventDefault();
-    // take user input and change to integer
+    // take user input and convert to integer
     var number = parseInt($("#input-number").val());
     $("#result").text("");
     var theResult = testPingPong(number);
