bcd4d7448205182cdb64fe9f22cce71413f439db
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-08-25 21:12:23 -0400

    remove JS function that clears text

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 // BUSINESS / BACK-END LOGIC
-var testPingPong = function(num) {
-  var lastNum = num;
+var testPingPong = function(maxValue) {
   var rangeArray = [];
 
   // DO NOTHING TO NON-NUMERIC INPUT AND NUMBERS LESS THAN 1.
-  if (num < 1 || isNaN(num)) {
+  if (maxValue < 1 || isNaN(maxValue)) {
     alert("Please enter a number (must be greater than 0)");
   }
   else {
-    for (var count = 1; count <= lastNum; count++) {
+    for (var count = 1; count <= maxValue; count++) {
       // NUMBERS DIVISIBLE BY 15 ARE REPLACED WITH "ping-pong".
       if (count % 15 === 0) {
         rangeArray.push("PING-PONG");
@@ -29,14 +28,13 @@ var testPingPong = function(num) {
     var result = rangeArray.join(", ");
     return result;
   }
-}
+};
 // USER INTERFACE / FRONT-END LOGIC
 $(document).ready(function() {
   $("#user-input").submit(function(event) {
     event.preventDefault();
-    var number = parseInt($("#input-number").val()); // takes user input and converts to integer
-    $("#result").text(""); // clears result area on screen and displays 'theResult' every time a user clicks submit
-    var rangeArrayresult = testPingPong(number); // variable that grabs what the function returns
+    var userInput = parseInt($("#input-number").val()); // takes user input and converts to integer
+    var rangeArrayresult = testPingPong(userInput); // variable that grabs what the function returns
     $("#result").text(rangeArrayresult); // displays rangeArrayresult
   });
 });
